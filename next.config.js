d204a54fcb0f607d5b8816f151f9c6e2675cc541
josecaros <josecarlos.42v@gmail.com>
2023-01-02 07:32:42 -0500

    basepath and prefix for resources

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images:{
-    loader:"akamai",
-    path:"",
-  }
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  basePath: "/nextjs-pages",
+  assetPrefix: "/nextjs-pages"
 }
 
 module.exports = nextConfig
